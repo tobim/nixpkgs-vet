4a91fcddc2ea8ea3d174539bb56afeac365041ce
Tobias Mayer <tobim@fastmail.fm>
2026-03-08 11:00:50 +0100

    Add shared ratchet infrastructure

--- src/ratchet.rs
+++ src/ratchet.rs
@@ -2,6 +2,8 @@
 //!
 //! Each type has a `compare` method that validates the ratchet checks for that item.
 
+use std::marker::PhantomData;
+
 use relative_path::RelativePath;
 use std::collections::BTreeMap;
 
@@ -102,6 +104,15 @@ pub trait ToProblem {
     fn to_problem(name: &str, optional_from: Option<()>, to: &Self::ToContext) -> Problem;
 }
 
+pub trait EnabledAttributeProblem {
+    fn introduced_problem(name: &str, file: RelativePathBuf) -> Problem;
+    fn regressed_problem(name: &str, file: RelativePathBuf) -> Problem;
+}
+
+/// The ratchet value of an evaluated boolean attribute that must be enabled for new packages and
+/// must not regress to `false` once enabled.
+pub struct EnabledAttribute<ProblemKind>(PhantomData<ProblemKind>);
+
 impl<Context: ToProblem> RatchetState<Context> {
     /// Compare the previous ratchet state of an attribute to the new state.
     /// The previous state may be `None` in case the attribute is new.
@@ -122,6 +133,18 @@ impl<Context: ToProblem> RatchetState<Context> {
             // - -> Tight (always okay to keep or make the ratchet tight)
             // - Anything involving NotApplicable, where we can't really make any good calls
             _ => Success(()),
+        }
+    }
+}
+
+impl<ProblemKind: EnabledAttributeProblem> ToProblem for EnabledAttribute<ProblemKind> {
+    type ToContext = RelativePathBuf;
+
+    fn to_problem(name: &str, optional_from: Option<()>, file: &Self::ToContext) -> Problem {
+        if optional_from.is_some() {
+            ProblemKind::regressed_problem(name, file.clone())
+        } else {
+            ProblemKind::introduced_problem(name, file.clone())
         }
     }
 }
